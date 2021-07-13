2a79b5191c599bdb157741db913897dbbab21677
Sergei Udris <sergei.udris@gmail.com>
2021-07-13 17:20:10 +0300

    Revert "Revert "remove license, auhtors, publisher, copyright, attribution headers, programmer names - repo is code only""
    This reverts commit 346b7c963f0a73320e8b0ec28c8ec618826148c7.

--- pedestal/hello-world/project.clj
+++ pedestal/hello-world/project.clj
@@ -1,20 +1,8 @@
-; Copyright 2013 Relevance, Inc.
-; Copyright 2014-2019 Cognitect, Inc.
 
-; The use and distribution terms for this software are covered by the
-; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
-; which can be found in the file epl-v10.html at the root of this distribution.
-;
-; By using this software in any fashion, you are agreeing to be bound by
-; the terms of this license.
-;
-; You must not remove this notice, or any other, from this software.
 
 (defproject hello-world "0.5.5"
   :description "Simple hello-world service in Pedestal"
   :url "http://pedestal.io/samples/index"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [io.pedestal/pedestal.service "0.5.5"]
                  [io.pedestal/pedestal.jetty "0.5.5"]
